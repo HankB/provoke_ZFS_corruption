57e9f89a90a1beee6b7824f5d3290fe25bb083fd
Hank Barta <hbarta@gmail.com>
2024-12-12 18:12:37 -0600

    3 deep filesystem structure

--- populate_pool.sh
+++ populate_pool.sh
@@ -12,11 +12,15 @@ pool=io_tank
 test_fs="test"
 
 zfs create "$pool/$test_fs"
-for l0 in 1 2 3             # 3 main (test) filesystems)
+for l0 in $(seq 0 3)             # 3 main (test) filesystems)
 do
     zfs create "$pool/$test_fs/l0_$l0"
-    for l1 in $(seq 0 5)    # 5 subs in each main 
+    for l1 in $(seq 0 3)    # 5 subs in each main 
     do
         zfs create "$pool/$test_fs/l0_$l0/l1_$l1"
+        for l2 in $(seq 0 3)    # 3 subs in each main 
+        do
+            zfs create "$pool/$test_fs/l0_$l0/l1_$l1/l2_$l2"
+        done
     done
 done
