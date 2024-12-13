baf6d7e95c8605ed505f1e68ed865479b3e0c92b
Hank Barta <hbarta@gmail.com>
2024-12-12 22:44:25 -0600

    more and compressed files

--- populate_pool.sh
+++ populate_pool.sh
@@ -4,11 +4,21 @@
 set -o errexit
 set -o pipefail
 set -o nounset
+set -x
 ############### end of Boilerplate
+
+# some tuning parameters
+max_timeout_seconds=10
+files_per_dir=20
 
 add_files() {
     cd "$1"
-    timeout 3 yes 123456789 > f || :
+    for fn in $(seq 1 $files_per_dir)
+    do
+        tmo=$((1 + RANDOM % "$max_timeout_seconds"))
+        timeout "$tmo" yes 0123456789 > "f_$fn" || :
+        xz -0 < "f_$fn"  > "f_$fn.xz"
+    done
 }
 
 # Create nested filesystems 3 layers deep.
@@ -24,9 +34,11 @@ do
     for l1 in $(seq 0 3)    # 5 subs in each main 
     do
         zfs create "$pool/$test_fs/l0_$l0/l1_$l1"
+        add_files "/mnt/$pool/$test_fs/l0_$l0/l1_$l1"
         for l2 in $(seq 0 3)    # 3 subs in each main 
         do
             zfs create "$pool/$test_fs/l0_$l0/l1_$l1/l2_$l2"
+            add_files "/mnt/$pool/$test_fs/l0_$l0/l1_$l1/l2_$l2"
         done
     done
 done
