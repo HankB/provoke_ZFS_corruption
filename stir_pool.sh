802578c8f991a986c78bb63c2289f8a91411d3f5
Hank Barta <hbarta@gmail.com>
2024-12-15 12:08:29 -0600

    work out skip logic

--- stir_pool.sh
+++ stir_pool.sh
@@ -4,18 +4,32 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-set -x
+#set -x
 ############### end of Boilerplate
 
 # see https://github.com/HankB/provoke_ZFS_corruption
 
 # tunables
 skip_range=20 # skip 0-20 files and modify 
+
 # following solution found at 
 # https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash
 
 mapfile -d $'\0' file_list < <(find /mnt/io_tank/test -type f -print0)
 echo
 echo ${#file_list[*]} files
-echo "${file_list[0]}"
-echo "${file_list[1]}"
+
+list_count=${#file_list[*]}
+file_index=0
+
+while [ "$file_index" -lt "$list_count" ]
+do
+    skip=$((RANDOM % skip_range))
+    file_index=$((file_index + skip))
+    if [ "$file_index" -ge "$list_count" ]
+    then
+        break;
+    fi
+    echo skip $skip to $file_index
+    echo "${file_list[$file_index]}"
+done
