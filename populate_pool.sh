980c5207e5b0eeed14a0a30e7aaefb656b58cb75
Hank Barta <hbarta@gmail.com>
2024-12-16 11:49:22 -0600

    tweak tunables, perms (incomplete)

--- populate_pool.sh
+++ populate_pool.sh
@@ -10,17 +10,18 @@ set -x
 # see https://github.com/HankB/provoke_ZFS_corruption
 
 # some tuning parameters
-max_timeout_seconds=10          # timeout for creating compressible files
-files_per_dir=20                # max file creation loops (2 files/loop)
-max_random_count=10             # block count for random files
-random_blk_size=1G            # max block size for random
+max_timeout_seconds=20          # timeout for creating compressible files
+files_per_dir=40                # max file creation loops (2 files/loop)
+max_random_count=20             # block count for random files
+random_blk_size=100K            # max block size for random
+user=hbarta
 
 add_files() {
     cd "$1"
     for fn in $(seq 1 $files_per_dir)
     do
         tmo=$((1 + RANDOM % "$max_timeout_seconds"))
-        timeout "$tmo" yes 0123456789 > "txt_$fn" || :
+        timeout "$tmo" yes 01234 > "txt_$fn" || :
         blocks=$((1 + RANDOM % "$max_random_count"))
         dd if=/dev/urandom bs=$random_blk_size count=$blocks of="rnd_$fn" 
     done
@@ -36,7 +37,7 @@ for l0 in $(seq 0 3)             # 3 main (test) filesystems)
 do
     zfs create "$pool/$test_fs/l0_$l0"
     add_files "/mnt/$pool/$test_fs/l0_$l0"
-    for l1 in $(seq 0 3)    # 5 subs in each main 
+    for l1 in $(seq 0 3)    # 3 subs in each main 
     do
         zfs create "$pool/$test_fs/l0_$l0/l1_$l1"
         add_files "/mnt/$pool/$test_fs/l0_$l0/l1_$l1"
@@ -44,6 +45,7 @@ do
         do
             zfs create "$pool/$test_fs/l0_$l0/l1_$l1/l2_$l2"
             add_files "/mnt/$pool/$test_fs/l0_$l0/l1_$l1/l2_$l2"
+            chown -R $user:$user "/mnt/$pool/$test_fs/l0_$l0/l1_$l1/l2_$l2"
         done
     done
 done
