f933c4bb41040fec3795525eaa456d78437b5106
Hank Barta <hbarta@gmail.com>
2025-02-04 10:24:46 +0000

    works on FreeBSD

--- scripts/do_syncoid.sh
+++ scripts/do_syncoid.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+# set -x
 start=$(/bin/date  +%Y-%m-%d-%H%M)
 start_s=$(/bin/date +%s)
 
-/bin/time -p /sbin/syncoid --recursive --no-privilege-elevation send/test recv/test >"/home/hbarta/logs/$start.syncoid.txt" 2>&1
+time -p syncoid --recursive --no-privilege-elevation send/test recv/test >"/home/hbarta/logs/$start.syncoid.txt" 2>&1
 
-finish_s=$(/bin/date +%s)
+finish_s=$(date +%s)
 elapsed=$((finish_s-start_s))
 zpool status send >>"/home/hbarta/logs/$start.syncoid.txt" 2>&1
 mv "/home/hbarta/logs/$start.syncoid.txt" "/home/hbarta/logs/$start.syncoid.$elapsed.txt"
