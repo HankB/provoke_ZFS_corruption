099897cbdcdf41dfc3be9349a9d4a7647cdc0627
HB <hbarta@gmail.com>
2025-02-06 09:47:11 -0600

    run scripts serially

---
+++ scripts/thrash_zfs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Use a file '/home/hbarta/logs/halt_test.txt' to stop the test
+# by writing the current time to that file.
+
+while(:)
+do
+    # check for halt condition
+    if [ -e /home/hbarta/logs/halt_test.txt ]
+    then
+        exit
+    fi
+
+    # thrash away and check for corruption reported
+    time -p do_stir.sh
+
+    # And check for corruption
+    # shellcheck disable=SC2012
+    # shellcheck disable=SC2046
+    if ( grep -q "use '-v' for a list" $(ls -t /home/hbarta/logs/*stir_pools*|tail ) )
+    then
+        date +%Y-%m-%d-%H%M >>/home/hbarta/logs/halt_test.txt
+        exit
+    fi
+
+    zfs snap -r send@$(date +%s).$(date +%Y-%m-%d-%H%M)
+    do_syncoid.sh
+    
+    # And check for corruption
+    # shellcheck disable=SC2012
+    # shellcheck disable=SC2046
+    if ( grep -q "use '-v' for a list" $(ls -t /home/hbarta/logs/*syncoid*|tail ) )
+    then
+        date +%Y-%m-%d-%H%M >>/home/hbarta/logs/halt_test.txt
+        exit
+    fi
+
+
+done
