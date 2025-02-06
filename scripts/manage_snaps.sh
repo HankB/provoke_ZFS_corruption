b90049c425e7a2d4e83b61de151f57d4edefc983
Hank Barta <hbarta@gmail.com>
2025-02-06 02:09:03 +0000

    loop snapshot management and exit on corruptiuon

---
+++ scripts/manage_snaps.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Use a file '/home/hbarta/logs/halt_test.txt' to stop the test
+# by writing the current time to that file.
+
+while(:)
+do
+    # check for halt condition
+    if [ -e /home/hbarta/logs/halt_test.txt ]
+    then
+        echo "halting on corruption detected"
+        exit
+    fi
+
+    do_trim_snaps.sh
+    
+    # And check for corruption
+    for log in $(find /home/hbarta/logs -type f|sort|tail)
+    do
+        if ( grep -q "use '-v' for a list" "$log" )
+        then
+            echo "corruption detected in $log"
+            date +%Y-%m-%d-%H%M >>/home/hbarta/logs/halt_test.txt
+            exit
+        fi
+    done
+
+    sleep 60
+
+done
