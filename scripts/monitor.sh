107d770ba6823935e13ffdf402b509b8104e0de6
Hank Barta <hbarta@gmail.com>
2025-02-12 16:52:27 -0600

    helper to report key statistics

---
+++ scripts/monitor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# package several monitoring processes in one script suitabnle for cmdwatch
+
+echo status
+zpool status
+echo
+echo list
+echo
+zpool list
+echo
+echo send snapshoput count
+zfs list -t snap -r send|wc -l
+echo
+echo recv snapshoput count
+zfs list -t snap -r recv|wc -l
+
